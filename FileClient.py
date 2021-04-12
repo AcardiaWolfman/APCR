019d8128d481a33d990a6382fa6746faad33221a
acardiaWolfman <acardiawolfman@pop-os.localdomain>
2021-04-11 20:29:16 -0500

    Finalización de envio por carpetas de archivos

--- FileClient.py
+++ FileClient.py
@@ -1,29 +1,37 @@
-import socket, os
+import socket, os, time
 
 class FileClient():
-  def ClientProcess(self, file_path, file_name):
+  def send_folder(self, TCPClientSocket, file_path, file_name):
+    print(f"Enviando archivo {file_name}...")
+    TCPClientSocket.sendall(bytes(f'{file_name}/{os.path.getsize(file_path)}','utf-8'))
+    confirmacion = TCPClientSocket.recv(self.buffer_size)
+    print(f"el servidor confirmo que esta listo: {confirmacion.decode('utf-8')}")
+    file_to_send = open(file_path, "rb")
+    print("El Servidor recibirá el archivo")
+    while True:
+      part_of_file = file_to_send.read(self.buffer_size)
+      if not part_of_file:
+        break
+      TCPClientSocket.sendall(part_of_file)
+    file_to_send.close()
+    print("Esperando confirmación del servidor")
+    confirmacion = TCPClientSocket.recv(self.buffer_size)
+    print(f'El servidor dice: {confirmacion.decode("utf-8")}')
+    print("terminando")
+
+  def ClientProcess(self):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
       TCPClientSocket.connect((self.HOST, self.PORT))
-      print(f"Enviando archivo {file_name}...")
-      TCPClientSocket.sendall(bytes(file_name,'utf-8'))
-      file_to_send = open(file_path, "rb")
-      print("El Servidor recibirá el archivo")
-      while True:
-        part_of_file = file_to_send.read(self.buffer_size)
-        if not part_of_file:
-          break
-        TCPClientSocket.sendall(part_of_file)
-      print("Enviando señal de fin")
-      file_to_send.close()
-      print("terminando")
-  def ProcessFolder(self):
+      self.ProcessFolder(TCPClientSocket)
+      TCPClientSocket.sendall(bytes('fin', 'utf-8'))
+  def ProcessFolder(self,TCPClientSocket):
     #path = input("Introduce la ruta de archivos a enviar")
     path = "/home/acardiawolfman/Documentos/filesToSend"
     for f in os.listdir(path):
-      self.ClientProcess(os.path.join(path, f), f)
+        self.send_folder(TCPClientSocket,os.path.join(path, f), f)
   def __init__(self):
     self.HOST = "127.0.0.1"  # The server's hostname or IP address
     self.PORT = 65432  # The port used by the server
     self.buffer_size = 1024
-    self.ProcessFolder()
+    self.ClientProcess()
 FileClient()
--- FileServer.py
+++ FileServer.py
@@ -10,18 +10,26 @@ class FileServer():
         while True:
             Client_conn, Client_addr = TCPServerSocket.accept()
             with Client_conn:
-                print("Cliente", Client_addr)
-                data = Client_conn.recv(self.buffer_size)
-                file_name = data.decode('utf-8')
-                print("Salvando "+file_name)
-                file_to_save = open(f"{datetime.datetime.today().strftime('%d-%m-%y-%I-%M-%S-%p')}_{file_name}","wb")
                 while True:
+                  print("Cliente", Client_addr)
+                  data = Client_conn.recv(self.buffer_size)
+                  file_data = data.decode('utf-8').split('/')
+                  file_name = file_data[0]
+                  if file_name == "fin" and len(file_data) == 1:
+                    break
+                  file_size = int(file_data[1])
+                  recived = 0
+                  print("Salvando " + file_name)
+                  file_to_save = open(f"{datetime.datetime.today().strftime('%d-%m-%y-%I-%M-%S-%p')}_{file_name}","wb")
+                  Client_conn.sendall(bytes("Listo", 'utf-8'))
+                  while recived < file_size:
                     data = Client_conn.recv(self.buffer_size)
-                    if  not data:
-                        break
                     file_to_save.write(data)
-                file_to_save.close()
-                print("Se guardo exitosamente el archivo, listo para recibir  nuevo\n")
+                    recived += self.buffer_size
+                    print(f"{recived} de {file_size}")
+                  file_to_save.close()
+                  print("Se guardo exitosamente el archivo, listo para recibir  nuevo\n")
+                  Client_conn.sendall(bytes('exito al guardar','utf-8'))
   def __init__(self):
     self.HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     self.PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
